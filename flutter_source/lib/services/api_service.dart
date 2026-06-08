61c5bcfa45e768a00983e0cff467b4169de9bed6
Ijlaal Akhtar <ijlaalakhtar@gmail.com>
2026-06-08 07:25:31 +0000

    Update API base URL to production domain

--- flutter_source/lib/services/api_service.dart
+++ flutter_source/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://10.0.2.2:8000/api/v1'; 
+  static const String baseUrl = 'https://staff.lakkadloha.com/api/v1'; 
   final _storage = const FlutterSecureStorage();
 
   Future<Map<String, String>> _getHeaders() async {
